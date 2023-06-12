97ba74f2e2ee94f6bda9f351cf5488ebeaeb2214
Venkatesan Chandra Sekaran <venkatesan_dinesh@icloud.com>
2023-06-12 12:05:42 +0530

    index is updated

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,30 +1,17 @@
 import React, { useEffect } from 'react'
 import Sidebar from './Sidebar'
 import TopBar from './Appbar'
+import './Style.css'
 export default function Dashboard() {
-  const PreventBackButton = () => {
-    useEffect(() => {
-      const disableBackButton = (e) => {
-        e.preventDefault();
-        window.location.replace(window.location.href);
-      };
-  
-      window.addEventListener('popstate', disableBackButton);
-  
-      return () => {
-        window.removeEventListener('popstate', disableBackButton);
-      };
-    }, []);
-  }  
   return (
     <div>
         <TopBar/>
         <Sidebar/>
-        <div style={{ textAlign: 'center', paddingTop: '200px'}}>
-      <h1>Welcome to My Website</h1>
-      <p>This is the welcome page of my Search Engine.</p>
+        <div className='container1'>
+         <h1>Welcome to My Website</h1>
+      <p>This is the welcome page of my Search Engine. </p>
       <p>Use search icon to select list of State</p>
-      <p>Feel free to explore and enjoy your stay!</p>
+     <p>Feel free to explore and enjoy your stay!</p>
     </div>
     </div>
   )
